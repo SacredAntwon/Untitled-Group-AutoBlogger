07609602342e563fa615124d13afac74c5ceed04
SacredAntwon <antwonm9898@gmail.com>
2022-10-30 15:11:22 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ window.onload = function(){
     var listAsStr ="<ul>" + list.join("") + "<ul>";
     var test = "I'm heading back to Colorado tomorrow after being down in Santa Barbara over the weekend for the festival there. I will be making October plans once there and will try to arrange so I'm back here for the birthday if possible. I'll let you know as soon as I know the doctor's appointment schedule and my flight plans.";
     document.getElementById("test").innerHTML = test;
-    document.getElementById("list").innerHTML = listAsStr;
+    //document.getElementById("list").innerHTML = listAsStr;
 }
