2558c4c7cf200ab139a01fda2c08151bea4a1afb
DTM1218 <dtm121898@gmail.com>
2022-11-07 00:26:01 -0800

    The website can now open archive posts

--- archive.mjs
+++ archive.mjs
@@ -1,14 +1,25 @@
-import {listFiles} from "./genPytoJs.mjs";
 import express from "express";
+import fs from "fs";
 
-export default function archive() {
-  const router = express.Router();
+export function archive() {
+    const router = express.Router();
 
-  router.get("/archive", function (req, res) {
-    // Call the function to get the list of items
-    let files = listFiles();
-    console.log("Archive");
-    res.render("archive", { file : files });
-  });
-  return router
+    router.get("/archive", function (req, res) {
+        // Call the function to get the list of items
+        let files = listFiles();
+        console.log("Archive");
+        res.render("archive", {file: files});
+    });
+    return router
+}
+
+// This will grab the list of files names from 'archive'
+export function listFiles() {
+    const files = fs.readdirSync('./archive');
+    const index = files.indexOf('.DS_Store');
+    // Removes DS_Store from the list
+    if (index > -1) {
+        files.splice(index, 1);
+    }
+    return files;
 }
--- router.mjs
+++ router.mjs
@@ -6,12 +6,19 @@ export default function startRouter() {
   checkArticle();
 
   router.get("/", function (req, res) {
-    // If page file gets removed while the website is up, it
-    // will generate another one.
-    checkArticle();
+    let jsonData = null
+    if (Object.keys(req.query).length !== 0 && typeof req.query.post != 'undefined') {
+      console.log(`User queried post from: ${req.query.post}`)
+      jsonData = req.query.post
+    }
+    if (jsonData === null) {
+      // If page file gets removed while the website is up, it
+      // will generate another one.
+      checkArticle();
+    }
 
     // Get the blog post information
-    let blogInfo = getBlogPost();
+    let blogInfo = getBlogPost(jsonData);
     
     console.log("Start page");
     res.render("index", { blog : blogInfo, picture : blogInfo.image });
