fb7fd273d0e4001d4eac25383c4c8037fc768a5c
SacredAntwon <antwonm9898@gmail.com>
2022-11-09 16:37:12 -0800

    Split gen into functions, centered stars
    There is also the beginning of a unit test for python

--- gen.py
+++ gen.py
@@ -12,38 +12,49 @@ from simple_image_download import simple_image_download as simp
 
 gen = DocumentGenerator()
 
-date_time = datetime.now()
-my_timezone=timezone('US/Pacific')
-fullDate = my_timezone.localize(date_time)
-fullDate = fullDate.astimezone(my_timezone)
-date = fullDate.strftime("%m_%d_%Y")
-time = fullDate.strftime("%H:%M")
+def getDateAndTime():
+    date_time = datetime.now()
+    my_timezone=timezone('US/Pacific')
+    fullDate = my_timezone.localize(date_time)
+    fullDate = fullDate.astimezone(my_timezone)
+    date = fullDate.strftime("%m_%d_%Y")
+    time = fullDate.strftime("%H:%M")
 
-word = gen.word()
-title = gen.sentence()
+    return date,fullDate,time
 
-article = ''
+def generateArticle():
+    word = gen.word()
+    title = gen.sentence()
+    date, fullDate, time = getDateAndTime()
 
-for i in range(7):
-    article += gen.paragraph() + ' '
+    article = ''
 
-response = simp.simple_image_download()
-image = response.urls(word, limit=10)[-1]
+    for i in range(7):
+        article += gen.paragraph() + ' '
 
-imageHeight = random.randint(290, 405)
-imageWidth = random.randint(290, 405)
+    response = simp.simple_image_download()
+    image = response.urls(word, limit=10)[-1]
 
-data = {
-    "date": str(fullDate.strftime("%m/%d/%Y")),
-    "time": str(time),
-    "article": str(article),
-    "title": str(title),
-    "author": str(names.get_full_name()),
-    "email": str(gen.email()),
-    "image": image,
-    "imageHeight": imageHeight,
-    "imageWidth": imageWidth
-}
+    imageHeight = random.randint(290, 405)
+    imageWidth = random.randint(290, 405)
 
-with open(f"archive/article_{date}.json", "w") as outfile:
-    json.dump(data, outfile)
+    data = {
+        "date": str(fullDate.strftime("%m/%d/%Y")),
+        "time": str(time),
+        "article": str(article),
+        "title": str(title),
+        "author": str(names.get_full_name()),
+        "email": str(gen.email()),
+        "image": image,
+        "imageHeight": imageHeight,
+        "imageWidth": imageWidth
+    }
+
+    return data, date
+
+def writeToJson():
+    data, date = generateArticle()
+    with open(f"archive/article_{date}.json", "w") as outfile:
+        json.dump(data, outfile)
+
+writeToJson()
